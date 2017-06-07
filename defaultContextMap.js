8ad764360f862ce2918f10cc50398bdd2ab529e9
loveKino <842914439@qq.com>
2017-06-07 19:08:40 +0800

    default context map

---
+++ defaultContextMap.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var dataContainer = require('./dataContainer');
+
+let MetaMethod = dataContainer.MetaMethod;
+
+// TODO system methods
+module.exports = {
+    '+': MetaMethod(function(v1, v2) {
+        return v1 + v2;
+    }),
+
+    '-': MetaMethod(function(v1, v2) {
+        return v1 - v2;
+    }),
+
+    '*': MetaMethod(function(v1, v2) {
+        return v1 * v2;
+    }),
+
+    '/': MetaMethod(function(v1, v2) {
+        return v1 / v2;
+    })
+};
